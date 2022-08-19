80929e8ac072020fe395f41f280523e7d10b7a8d
widyaput <13519105@std.stei.itb.ac.id>
2022-08-19 17:02:54 +0700

    Update login page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,123 @@
+import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
+import APIClient from '@/utils/api-client';
+import AuthService from '@/service/auth';
+import Logo from '@assets/images/logo.png';
+import Spinner from '@/components/Spinner';
+
+const Login: React.FC<{ setState: Function }> = ({
+    setState,
+}: {
+    setState: Function;
+}) => {
+    const [loading, setLoading] = useState(false);
+    const [errors, setErrors] = useState({
+        username: '',
+        password: '',
+    });
+    const redirectIfHaveToken: Function = async () => {
+        const token = await APIClient.checkToken();
+        if (Object.keys(token).length > 0) {
+            setState(true);
+        }
+    };
+
+    const onFinish = async (e: any) => {
+        const username = e.target[0].value;
+        const password = e.target[1].value;
+        if (!username || !password) {
+            setErrors({
+                username: !username ? 'Username is required' : '',
+                password: !password ? 'Password is required' : '',
+            });
+            e.preventDefault();
+            return;
+        }
+        setLoading(true);
+        await AuthService.login(
+            username,
+            password,
+            (response) => {
+                toast.success(`Login Successfull (${response.user.username})`);
+                setState(true);
+                setLoading(false);
+            },
+            (error) => {
+                if (error.toString().includes('Invalid identifier')) {
+                    toast.error(
+                        'NIM/No. Registrasi atau password salah atau tidak ditemukan!'
+                    );
+                } else {
+                    toast.error(error.toString());
+                }
+                setState(false);
+                setLoading(false);
+            }
+        );
+    };
+
+    useEffect(() => {
+        redirectIfHaveToken();
+    }, []);
+
+    if (loading) {
+        return <Spinner />;
+    }
+
+    return (
+        <div className="flex justify-center items-center h-screen bg-Blue">
+            <div className="basis-3/4 md:basis-7/12 md:scale-[1.3] lg:max-w-lg bg-white">
+                <div className="rounded-lg shadow-lg border-0">
+                    <div className="flex justify-center mb-3 md:mb-5 flex-wrap">
+                        <div className="w-1/2">
+                            <img
+                                src={Logo}
+                                className="shadow rounded-full max-w-full h-auto align-middle border-none"
+                                alt="logo"
+                            />
+                        </div>
+                    </div>
+                    <form onSubmit={onFinish}>
+                        <label
+                            className="block mb-2 text-sm text-gray-700 font-caption"
+                            htmlFor="username"
+                        >
+                            <input
+                                id="username"
+                                name="username"
+                                placeholder="NIM/No. Registrasi"
+                                className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:border-blue-500"
+                            />
+                            <span className="text-red-500">
+                                {errors.username}
+                            </span>
+                        </label>
+
+                        <label
+                            className="block mb-2 text-sm text-gray-700 font-caption"
+                            htmlFor="password"
+                        >
+                            <input
+                                name="password"
+                                type="password"
+                                placeholder="Password"
+                                className="w-full px-3 py-2 border rounded-lg border-gray-300 focus:outline-none focus:border-blue-500"
+                            />
+                            <span className="text-red-500">
+                                {errors.password}
+                            </span>
+                        </label>
+                        <button
+                            type="submit"
+                            className="w-full text-black bg-Gray focus:ring-4 focus:outline-none hover:bg-Blue hover:text-dark"
+                        >
+                            <span className="w-48">Sign In</span>
+                        </button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
